12475c9ef201ffb0e3d46eadaa3b13317a6a0d4c
mac <@b.tunde.ipaye@gmail.com>
2026-07-27 15:46:41 +0100

    feat: app routes updated commit by ENGR. IPAYE

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -1,11 +1,11 @@
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, Navigate } from "react-router-dom"
 
 const AppRoutes = () =>{
     return(
         <Routes>
             <Route path="/" element={<span>HOME PAGE</span>} />
             <Route path="/user-profile" element={<span>USER PROFILE PAGE</span>} />
-
+            <Route path="*" element={<Navigate to ="/"/>} />
         </Routes>
     );
 };

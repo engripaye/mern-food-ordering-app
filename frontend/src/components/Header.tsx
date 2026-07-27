853333a051fd76956abf92d253d12626f3f77d04
mac <@b.tunde.ipaye@gmail.com>
2026-07-27 16:50:25 +0100

    feat: header page updated commit by ENGR. IPAYE

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,6 +1,7 @@
 const Header = () => {
   return (
-      <div className="border-b-2 border-b-orange-500">
+      <div className="border-b-2 border-b-orange-500 py-6">
+          <div className="container mx-auto flex justify-between items-center"></div>
 
       </div>
   )

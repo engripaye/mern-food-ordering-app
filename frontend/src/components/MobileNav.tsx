ee51478c3864886a2aca7061e0b754c9e8992017
mac <@b.tunde.ipaye@gmail.com>
2026-07-27 17:24:44 +0100

    feat: Mobile Nav, Menu page updated commit by ENGR. IPAYE

--- frontend/src/components/MobileNav.tsx
+++ frontend/src/components/MobileNav.tsx
@@ -1,9 +1,12 @@
-import { Sheet } from "./ui/sheet";
+import {Sheet, SheetTrigger} from "./ui/sheet";
+import {Menu} from "lucide-react";
 
 const MobileNav = () => {
     return (
         <Sheet>
-
+            <SheetTrigger>
+                <Menu className="text-orange-500"/>
+            </SheetTrigger>
         </Sheet>
     )
 

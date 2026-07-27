deab2de1d530add08098cb9b7a74346703da5b2e
mac <@b.tunde.ipaye@gmail.com>
2026-07-27 17:48:29 +0100

    feat: Main Nav page frontend updated commit by ENGR. IPAYE

--- frontend/src/components/MainNav.tsx
+++ frontend/src/components/MainNav.tsx
@@ -3,7 +3,9 @@ import {Button} from "@/components/ui/button.tsx";
 const MainNav = () => {
 
     return(
-        <Button variant="ghost" className="font-bold hover:text-orange-500">
+        <Button variant="ghost"
+                className="font-bold hover:text-orange-500 hover:bg-white">
+            Log In
 
         </Button>
     )

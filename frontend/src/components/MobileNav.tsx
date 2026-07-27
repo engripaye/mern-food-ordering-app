8297fe08642c6adc46553ac8380a4f97c1d2ff6d
mac <@b.tunde.ipaye@gmail.com>
2026-07-27 17:29:43 +0100

    feat: Mobile Nav page updated commit by ENGR. IPAYE

--- frontend/src/components/MobileNav.tsx
+++ frontend/src/components/MobileNav.tsx
@@ -15,7 +15,7 @@ const MobileNav = () => {
                 </SheetTitle>
                 <Separator />
                 <SheetDescription className="flex">
-                    <Button className="flex-1 font-bold bg-orange-500"></Button>
+                    <Button className="flex-1 font-bold bg-orange-500">Log In</Button>
                 </SheetDescription>
             </SheetContent>
         </Sheet>

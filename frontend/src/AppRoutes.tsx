62d02f86e541385ffb72f5abbddbf67b3b26f2cf
mac <@b.tunde.ipaye@gmail.com>
2026-07-27 15:44:04 +0100

    feat: app routes commit by ENGR. IPAYE

---
+++ frontend/src/AppRoutes.tsx
@@ -0,0 +1,9 @@
+import { Routes, Route } from "react-router-dom"
+
+const AppRoutes = () =>{
+    return(
+        <Routes>
+            <Route path="/" element={<span>HOME PAGE</span>} />
+        </Routes>
+    )
+}

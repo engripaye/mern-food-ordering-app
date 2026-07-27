1a69e83e27b67c49fc746be925b362e7da558c55
mac <@b.tunde.ipaye@gmail.com>
2026-07-27 17:29:00 +0100

    feat: Mobile Nav page updated commit by ENGR. IPAYE

--- frontend/src/components/MobileNav.tsx
+++ frontend/src/components/MobileNav.tsx
@@ -1,5 +1,7 @@
-import {Sheet, SheetTrigger} from "./ui/sheet";
+import {Sheet, SheetContent, SheetDescription, SheetTitle, SheetTrigger} from "./ui/sheet";
 import {Menu} from "lucide-react";
+import {Separator} from "@radix-ui/react-separator";
+import {Button} from "@/components/ui/button.tsx";
 
 const MobileNav = () => {
     return (
@@ -7,6 +9,15 @@ const MobileNav = () => {
             <SheetTrigger>
                 <Menu className="text-orange-500"/>
             </SheetTrigger>
+            <SheetContent>
+                <SheetTitle>
+                    <span> Welcome to LetsEat.com </span>
+                </SheetTitle>
+                <Separator />
+                <SheetDescription className="flex">
+                    <Button className="flex-1 font-bold bg-orange-500"></Button>
+                </SheetDescription>
+            </SheetContent>
         </Sheet>
     )
 

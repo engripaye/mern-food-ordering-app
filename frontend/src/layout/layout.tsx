12ca24edbdbc377d8bac3fa66e93f1b467b801db
mac <@b.tunde.ipaye@gmail.com>
2026-07-27 16:22:51 +0100

    feat: react layout page updated commit by ENGR. IPAYE

--- frontend/src/layout/layout.tsx
+++ frontend/src/layout/layout.tsx
@@ -3,6 +3,15 @@ type Props = {
 }
 
 const Layout = ({ children }: Props) => {
+    return (
+        <div className="flex flex-col min-h-screen">
+            <Header  />
+
+            <div className="container mx-auto flex-1 py-10">
+                {children}
+            </div>
+        </div>
+    )
 
 }
 

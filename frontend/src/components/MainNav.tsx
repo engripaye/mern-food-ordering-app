766b19defea4d5d7ff5275d23500fde516d9781d
mac <@b.tunde.ipaye@gmail.com>
2026-07-27 17:46:05 +0100

    feat: Mobile Nav page updated commit by ENGR. IPAYE

---
+++ frontend/src/components/MainNav.tsx
@@ -0,0 +1,12 @@
+import {Button} from "@/components/ui/button.tsx";
+
+const MainNav = () => {
+
+    return(
+        <Button variant="ghost" className="font-bold hover:">
+
+        </Button>
+    )
+}
+
+export default MainNav

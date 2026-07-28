e23897bdbf77dcaae0d2cc596662123e5871209a
mac <@b.tunde.ipaye@gmail.com>
2026-07-28 01:14:59 +0100

    feat: Header updated commit by ENGR. IPAYE

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
               <Link
                   to="/"
                   className="text-3xl font-bold tracking-tight text-orange-500">
-                  LetsEat.com
+                  Let'sEat.com
               </Link>
 
               <div className="md:hidden">

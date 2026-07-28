f8b47c317cb2da96e331d7fd867979d55773146d
mac <@b.tunde.ipaye@gmail.com>
2026-07-28 23:31:54 +0100

    feat: Home page and images import updated commit by ENGR. IPAYE

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
 
     return (
     <div className="bg-orange-500 py-10">
-        <div className="container mx-auto flex flex-col md:flow-row justify-between items-center">
+        <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
             <span className="text-3xl text-white font-bold tracking-tight">
                 Let'sEat.com
             </span>

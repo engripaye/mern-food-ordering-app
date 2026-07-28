bad652ded66df5a093e576c5df4468415143067d
mac <@b.tunde.ipaye@gmail.com>
2026-07-28 01:11:36 +0100

    feat: Home page updated commit by ENGR. IPAYE

--- frontend/src/layout/layout.tsx
+++ frontend/src/layout/layout.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/Header.tsx";
+import Hero from "@/components/Hero.tsx";
 
 type Props = {
   children: React.ReactNode;
@@ -8,7 +9,7 @@ const Layout = ({ children }: Props) => {
     return (
         <div className="flex flex-col min-h-screen">
             <Header  />
-
+            <Hero />
             <div className="container mx-auto flex-1 py-10">
                 {children}
             </div>

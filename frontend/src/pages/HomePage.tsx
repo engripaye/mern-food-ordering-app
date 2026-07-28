292f9b654355c5474f943dd23429550579680ae2
mac <@b.tunde.ipaye@gmail.com>
2026-07-28 23:13:10 +0100

    feat: Home page and images import updated commit by ENGR. IPAYE

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,3 +1,6 @@
+import landingImage from "../assets/landing.png";
+import appDownloadImage from "../assets/appDownload.png";
+
 const HomePage = () =>{
   return (
       <div className="flex flex-col gap-12">
@@ -7,6 +10,18 @@ const HomePage = () =>{
             </h1>
             <span className="text-xl">Food is just a click away!</span>
         </div>
+          <div className="grid md:grid-cols-2 gap-5">
+              <img src={landingImage} />
+              <div className="flex flex-col items-center justify-center gap-4 text-center">
+                  <span className="font-bold text-3xl trcaking-tighter">
+                      Order takeaway even faster!
+                  </span>
+                  <span>
+                      Download the Let'sEat App for faster ordering and personalized recommendation
+                  </span>
+                  <img src={appDownloadImage} />
+              </div>
+          </div>
       </div>
   )
 }

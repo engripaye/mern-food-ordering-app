7a0e52b9934f6c9df7bbaa930a4f1e0e8618ca31
mac <@b.tunde.ipaye@gmail.com>
2026-07-29 04:55:19 +0100

    feat: Footer page updated commit by ENGR. IPAYE

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
                 Let'sEat.com
             </span>
             <span className="text-white font-bold tracking-tight flex gap-4">
-                <span> Privacy Policy</span>
+                <span> Privacy Policy </span>
                 <span> Terms of Service </span>
 
             </span>

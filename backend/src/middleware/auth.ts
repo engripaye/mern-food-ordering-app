73ed1761e3aedea5f451a6d236c381247a06f284
mac <@b.tunde.ipaye@gmail.com>
2026-08-11 07:50:02 +0100

    feat: auth express updated commit by ENGR. IPAYE

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -1,9 +1,11 @@
 import {auth} from "express-oauth2-jwt-bearer";
 
 export const jwtCheck = auth({
-    audience: 'mern-food-ordering-app-api',
-    issuerBaseURL: 'https://dev-ar51zhzxhw8v8cp2.us.auth0.com/',
+    audience: process.env.AUTH0_AUDIENCE,
+    issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
     tokenSigningAlg: 'RS256'
 });
+
+
 
 

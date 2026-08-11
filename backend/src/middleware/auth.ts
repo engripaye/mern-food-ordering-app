30e4b2142c93e6913e7d4759694c3809b585c148
mac <@b.tunde.ipaye@gmail.com>
2026-08-11 08:22:42 +0100

    feat: authenticated user profile endpoint backend sync updated commit by ENGR. IPAYE

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -1,10 +1,21 @@
 import {auth} from "express-oauth2-jwt-bearer";
-
+import {Request, Response, NextFunction} from "express";
 export const jwtCheck = auth({
     audience: process.env.AUTH0_AUDIENCE,
     issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
     tokenSigningAlg: 'RS256'
 });
+
+export const jwtParse = async (req: Request, res: Response, next: NextFunction) => {
+    const { authorization } = req.headers;
+
+    if (!authorization || !authorization.startsWith('Bearer ')) {
+        return res.status(401);
+    }
+
+    // bearer --token
+    const token = authorization.split(" ")[1];
+}
 
 
 

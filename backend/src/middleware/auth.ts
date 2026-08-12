9a94f8bb7c2adfc569c00d24dcec3d6c983d1faa
mac <@b.tunde.ipaye@gmail.com>
2026-08-12 02:27:19 +0100

    feat: auth0Id updated commit by ENGR. IPAYE

--- backend/src/middleware/auth.ts
+++ backend/src/middleware/auth.ts
@@ -1,6 +1,10 @@
 import {auth} from "express-oauth2-jwt-bearer";
 import {Request, Response, NextFunction} from "express";
 import jwt from 'jsonwebtoken';
+import User from "../model/user";
+
+declare global
+
 export const jwtCheck = auth({
     audience: process.env.AUTH0_AUDIENCE,
     issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
@@ -19,6 +23,17 @@ export const jwtParse = async (req: Request, res: Response, next: NextFunction) 
 
     try{
         const decoded = jwt.decode(token) as jwt.JwtPayload;
+        const auth0Id = decoded.sub;
+
+        const user = await User.find({ auth0Id });
+
+        if(!user){
+            return res.sendStatus(401);
+        }
+
+        req.auth0Id = auth0Id;
+        req.userId = user._id.toString();
+        next();
     }catch (error){
         return res.status(401);
     }

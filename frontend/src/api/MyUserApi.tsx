17820bc37acd458a7e9917619ab08366502965f9
mac <@b.tunde.ipaye@gmail.com>
2026-08-12 14:17:26 +0100

    feat: user api, error updated commit by ENGR. IPAYE

--- frontend/src/api/MyUserApi.tsx
+++ frontend/src/api/MyUserApi.tsx
@@ -1,6 +1,7 @@
 import {useMutation, useQuery} from "@tanstack/react-query";
 import {useAuth0} from "@auth0/auth0-react";
 import {toast} from "sonner";
+import {get} from "react-hook-form";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
@@ -24,7 +25,11 @@ export const useGetMyUser = () => {
         return response.json();
     };
 
-    const { data: currentUser, isLoding, error } = useQuery("fetchCurrentUser", )
+    const { data: currentUser, isLoding, error } = useQuery("fetchCurrentUser", getMyUserRequest);
+
+    if (error) {
+        toast.error(error.toString());
+    }
 };
 
 type CreateUserRequest = {

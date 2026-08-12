79b695457ccc1e6a59f7ce6d748fdcbdf69ab1b6
mac <@b.tunde.ipaye@gmail.com>
2026-08-12 02:43:26 +0100

    feat: validation request updated commit by ENGR. IPAYE

--- backend/src/routes/MyUserRoute.ts
+++ backend/src/routes/MyUserRoute.ts
@@ -1,10 +1,10 @@
 import express from "express";
 import MyUserController from "../controllers/MyUserController";
-import { jwtCheck, jwtParse } from "../middleware/auth";
+import { jwtCheck } from "../middleware/auth";
 
 const router = express.Router();
 
 router.post("/", jwtCheck, MyUserController.createCurrentUser);
-router.put("/", jwtCheck, jwtParse, MyUserController.updateCurrentUser);
+router.put("/", jwtCheck, MyUserController.updateCurrentUser);
 
 export default router;

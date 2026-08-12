eb61607d192223b2190b3187fb6f128da64e9b43
mac <@b.tunde.ipaye@gmail.com>
2026-08-12 13:28:39 +0100

    feat: user request updated  commit by ENGR. IPAYE

--- frontend/src/api/MyUserApi.tsx
+++ frontend/src/api/MyUserApi.tsx
@@ -56,6 +56,7 @@ export const useUpdateMyUser=() => {
     const { getAccessTokenSilently } = useAuth0();
 
     const updateMyUserRequest = async (formData: UpdateMyUserRequest)=>{
+        const accessToken = await getAccessTokenSilently();
         const response = await fetch(`${API_BASE_URL}/api/my/user`, {
             method: 'POST',
             headers: {
@@ -68,5 +69,8 @@ export const useUpdateMyUser=() => {
             throw new Error("Failed to update user");
         }
 
-    }
-}
+        return response.json()
+
+    };
+
+};

1f9e3ea1681d28dd00fb8e3339c1b95267b29c29
mac <@b.tunde.ipaye@gmail.com>
2026-08-12 03:11:23 +0100

    feat: user form profile form updated commit by ENGR. IPAYE

--- frontend/src/forms/user-profile-form/UserProfileForm.tsx
+++ frontend/src/forms/user-profile-form/UserProfileForm.tsx
@@ -1,4 +1,6 @@
 import { z } from "zod";
+import {useForm} from "react-hook-form";
+import {zodResolver} from "@hookform/resolvers/zod";
 
 
 const formSchema = z.object({
@@ -7,4 +9,21 @@ const formSchema = z.object({
     addressLine1: z.string().min(1, "Address Line is required"),
     city: z.string().min(1, "City is required"),
     country: z.string().min(1, "Country is required"),
-})
+});
+
+type UserFormData = z.infer<typeof formSchema>;
+
+type Props = {
+    onSave: (userProfileData: UserFormData) => void;
+    isLoading: boolean;
+
+}
+
+const UserProfileForm = ({ onSave, isLoading}: Props) => {
+    const form = useForm<UserFormData>({
+        resolver: zodResolver(formSchema)
+    });
+
+}
+
+export default UserProfileForm

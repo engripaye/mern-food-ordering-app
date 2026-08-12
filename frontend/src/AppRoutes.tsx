eb9db02cd44f4416f9823646337e5d9d8a7b3709
mac <@b.tunde.ipaye@gmail.com>
2026-08-12 14:47:06 +0100

    feat: user app protected route updated commit by ENGR. IPAYE

--- frontend/src/AppRoutes.tsx
+++ frontend/src/AppRoutes.tsx
@@ -2,15 +2,21 @@ import { Routes, Route, Navigate } from "react-router-dom"
 import Layout from "./layout/layout";
 import AuthCallbackPage from "@/pages/AuthCallbackPage.tsx";
 import UserProfilePage from "@/pages/UserProfilePage.tsx";
+import ProtectedRoute from "@/auth/ProtectedRoute.tsx";
 
 const AppRoutes = () =>{
     return(
         <Routes>
             <Route path="/auth-callback" element={<AuthCallbackPage/>} />
-            <Route path="/user-profile"
-                   element={<Layout showHero>
-                <UserProfilePage />
-            </Layout>} />
+
+            <Route element={<ProtectedRoute />}>
+
+                <Route path="/user-profile"
+                       element={<Layout showHero>
+                           <UserProfilePage />
+                       </Layout>} />
+            </Route>
+
             <Route path="*" element={<Navigate to ="/"/>} />
         </Routes>
     );

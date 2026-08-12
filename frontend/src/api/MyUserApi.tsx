e3cd1290aeb5901bfee23c3158e7b3bafab769ee
mac <@b.tunde.ipaye@gmail.com>
2026-08-12 13:47:15 +0100

    feat: userApi updated commit by ENGR. IPAYE

--- frontend/src/api/MyUserApi.tsx
+++ frontend/src/api/MyUserApi.tsx
@@ -1,5 +1,6 @@
 import {useMutation} from "@tanstack/react-query";
 import {useAuth0} from "@auth0/auth0-react";
+import {toast} from "sonner";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
@@ -74,6 +75,14 @@ export const useUpdateMyUser=() => {
     };
 
     const { mutateAsync: updateUser, isLoading, isSuccess, isError, error, reset } = useMutation(updateMyUserRequest);
+
+    if(isSuccess){
+        toast.success("User profile updated successfully!");
+    }
+
+    if(error){
+        toast.error(error.toString());
+    }
 
     return{
         updateUser,

37da96f14e784199a98aa5a285d7596c7b6f1358
mac <@b.tunde.ipaye@gmail.com>
2026-08-12 02:50:44 +0100

    feat: user routes updated commit by ENGR. IPAYE

--- backend/src/routes/MyUserRoute.ts
+++ backend/src/routes/MyUserRoute.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import MyUserController from "../controllers/MyUserController";
 import { jwtCheck } from "../middleware/auth";
+import {validateMyUserRequest} from "../middleware/validation";
 
 const router = express.Router();
 
 router.post("/", jwtCheck, MyUserController.createCurrentUser);
-router.put("/", jwtCheck, MyUserController.updateCurrentUser);
+router.put("/", jwtCheck, validateMyUserRequest, MyUserController.updateCurrentUser);
 
 export default router;
